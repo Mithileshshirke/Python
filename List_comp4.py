0a06621da9d11e7922615b20185cb1d0e879290a
Mithilesh Shirke <82035529+Mithileshshirke@users.noreply.github.com>
2021-08-01 16:08:00 +0530

    Delete List_comp4.py

--- List_comp4.py
+++
@@ -1,6 +0,0 @@
-# Store all numbers divisible by 5 from the given dictionary
-d={"a":15,"b":20,"c":31,"d":46,"e":65}#given dictionary
-L=[i for i in d.values() if i%5==0 ] #list comprehension
-#values() is inbuilt method use to access all values of keys.
-#condition check if number is divisible by 5 we use % to find remainder value
-print(L)
